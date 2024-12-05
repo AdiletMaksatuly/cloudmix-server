85516b19482a19bec66f1cdadabeaf3b8e7aeeb1
AdiletMaksatuly <bakeryamaker1@gmail.com>
2024-12-05 06:38:35 +0500

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import * as process from "node:process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,12 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies to be sent
+  });
 
   await app.listen(3000);
 }
